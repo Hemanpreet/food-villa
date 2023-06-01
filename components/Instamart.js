9d05a384ce340495c2f083b8bdac4899d069787b
Hemanpreet <hemanpreet7@gmail.com>
2023-06-01 23:25:49 +0530

    css using tailwind in parcel and complete auto collapse accordian functionality

--- components/Instamart.js
+++ components/Instamart.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Section = ({title,description,isVisible,setIsVisible}) => {
+const Section = ({ title, description, isVisible, setIsVisibleCallback }) => {
   return (
     <div className="border border-blue-600 border-2 p-2 m-2">
       <h3 className="font-bold text-xl">{title}</h3>
@@ -9,7 +9,7 @@ const Section = ({title,description,isVisible,setIsVisible}) => {
         <button
           className="cursor-pointer underline"
           onClick={() => {
-            setIsVisible(false);
+            setIsVisibleCallback(false);
           }}
         >
           Hide
@@ -18,7 +18,7 @@ const Section = ({title,description,isVisible,setIsVisible}) => {
         <button
           className="cursor-pointer underline"
           onClick={() => {
-            setIsVisible(true);
+            setIsVisibleCallback(true);
           }}
         >
           Show
@@ -30,6 +30,13 @@ const Section = ({title,description,isVisible,setIsVisible}) => {
 };
 
 const Instamart = () => {
+  function setSection(isVisible, Section) {
+    if (isVisible === true) {
+      setVisibleSection(Section);
+    } else {
+      setVisibleSection("");
+    }
+  }
   const [visibleSection, setVisibleSection] = useState("about");
   return (
     <div>
@@ -39,24 +46,24 @@ const Instamart = () => {
         description={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         }
-     isVisible={visibleSection==="about"}
-    setIsVisible={()=>setVisibleSection("about")}
+        isVisible={visibleSection === "about"}
+        setIsVisibleCallback={(bool) => setSection(bool, "about")}
       />
       <Section
         title={"Team Instamart"}
         description={
           "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur"
         }
-        isVisible={visibleSection==="team"}
-        setIsVisible={()=>setVisibleSection("team")}
+        isVisible={visibleSection === "team"}
+        setIsVisibleCallback={(bool) => setSection(bool, "team")}
       />
       <Section
         title={"Careers"}
         description={
           "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur"
         }
-        isVisible={visibleSection==="careers"}
-        setIsVisible={()=>setVisibleSection("careers")}
+        isVisible={visibleSection === "careers"}
+        setIsVisibleCallback={(bool) => setSection(bool, "careers")}
       />
     </div>
   );
